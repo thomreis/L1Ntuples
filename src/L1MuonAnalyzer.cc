824e7a52d34312fb64f915a515ff5aaea213f9ec
Thomas Reis <thomas.reis@cern.ch>
2015-12-04 08:45:16 +0100

    Add GMT histos to L1MuonAnalyzer

--- src/L1MuonAnalyzer.cc
+++ src/L1MuonAnalyzer.cc
@@ -13,10 +13,12 @@
 
 #include "DataFormats/L1Trigger/interface/Muon.h"
 #include "DataFormats/L1TMuon/interface/RegionalMuonCandFwd.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutRecord.h"
 // output
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "TTree.h"
+#include "TH2F.h"
 #include "L1Trigger/L1TMuon/interface/MuonRawDigiTranslator.h"
 #include "L1Trigger/L1TMuon/interface/RegionalMuonRawDigiTranslator.h"
 #include "L1Trigger/L1TMuon/interface/MicroGMTConfiguration.h"
@@ -33,7 +35,7 @@ public:
 
 MuonHistos::MuonHistos(edm::Service<TFileService> &fs, const std::string& label, const std::string& titlePrefix)
 {
-  hMuPerBx_ = fs->make<TH1I>(("hMuPerBx"+label).c_str(), (titlePrefix+" muons per BX").c_str(), 15, -7, 8);
+  hMuPerBx_ = fs->make<TH1I>(("hMuPerBx"+label).c_str(), (titlePrefix+" muons per BX;BX").c_str(), 15, -7, 8);
 }
 
 void
@@ -55,6 +57,7 @@ public:
   TH1I *hHwSign_;
   TH1I *hHwSignValid_;
   TH1I *hHwQual_;
+  TH2F *h2EtaPhi_;
 
   void FillMuon(const l1t::RegionalMuonCand& muon);
 };
@@ -68,6 +71,7 @@ TfHistos::TfHistos(edm::Service<TFileService> &fs, const std::string& label, con
   hHwSign_ = fs->make<TH1I>(("hHwSign"+label).c_str(), (titlePrefix+" hwSign").c_str(), 3, -1, 2);
   hHwSignValid_ = fs->make<TH1I>(("hHwSignValid"+label).c_str(), (titlePrefix+" hwSignValid").c_str(), 2, 0, 2);
   hHwQual_ = fs->make<TH1I>(("hHwQual"+label).c_str(), (titlePrefix+" hwQual").c_str(), 16, 0, 16);
+  h2EtaPhi_ = fs->make<TH2F>(("h2EtaPhi"+label).c_str(), (titlePrefix+" EtaPhi;#eta;#phi").c_str(), 100, -2.5, 2.5, 128, 0., 6.4);
 }
 
 void
@@ -76,10 +80,12 @@ TfHistos::FillMuon(const l1t::RegionalMuonCand& muon)
   hHwPt_->Fill(muon.hwPt());
   hHwEta_->Fill(muon.hwEta());
   hHwPhi_->Fill(muon.hwPhi());
-  hHwGlobalPhi_->Fill(l1t::MicroGMTConfiguration::calcGlobalPhi(muon.hwPhi(), muon.trackFinderType(), muon.processor()));
+  int globalPhi = l1t::MicroGMTConfiguration::calcGlobalPhi(muon.hwPhi(), muon.trackFinderType(), muon.processor());
+  hHwGlobalPhi_->Fill(globalPhi);
   hHwSign_->Fill(muon.hwSign());
   hHwSignValid_->Fill(muon.hwSignValid());
   hHwQual_->Fill(muon.hwQual());
+  h2EtaPhi_->Fill(muon.hwEta()*0.010875, globalPhi*0.010908);
 }
 
 
@@ -95,6 +101,7 @@ public:
   TH1I *hHwChargeValid_;
   TH1I *hHwQual_;
   TH1I *hHwIso_;
+  TH2F *h2EtaPhi_;
 
   void FillMuon(const l1t::Muon& muon);
 };
@@ -108,6 +115,7 @@ UGmtHistos::UGmtHistos(edm::Service<TFileService> &fs, const std::string& label,
   hHwChargeValid_ = fs->make<TH1I>(("hHwChargeValid"+label).c_str(), (titlePrefix+" hwChargeValid").c_str(), 2, 0, 2);
   hHwQual_ = fs->make<TH1I>(("hHwQual"+label).c_str(), (titlePrefix+" hwQual").c_str(), 16, 0, 16);
   hHwIso_ = fs->make<TH1I>(("hHwIso"+label).c_str(), (titlePrefix+" hwIso").c_str(), 4, 0, 4);
+  h2EtaPhi_ = fs->make<TH2F>(("h2EtaPhi"+label).c_str(), (titlePrefix+" EtaPhi;#eta;#phi").c_str(), 100, -2.5, 2.5, 128, 0., 6.4);
 }
 
 void
@@ -120,6 +128,7 @@ UGmtHistos::FillMuon(const l1t::Muon& muon)
   hHwChargeValid_->Fill(muon.hwChargeValid());
   hHwQual_->Fill(muon.hwQual());
   hHwIso_->Fill(muon.hwIso());
+  h2EtaPhi_->Fill(muon.hwEta()*0.010875, muon.hwPhi()*0.010908);
 }
 
 
@@ -144,14 +153,18 @@ private:
   edm::InputTag emtfTag_;
   edm::InputTag ugmtEmulatorTag_;
   edm::InputTag ugmtUnpackerTag_;
+  edm::InputTag gmtTag_;
 
   bool debug_;
+
+  bool suppressCommas_;
 
   bool analyzeBmtf_;
   bool analyzeOmtf_;
   bool analyzeEmtf_;
   bool analyzeUgmtEmulator_;
   bool analyzeUgmtUnpacker_;
+  bool analyzeGmt_;
 
   // output file
   edm::Service<TFileService> fs_;
@@ -170,6 +183,9 @@ private:
   TH1I *hMuChargeValidDiff_;
   TH1I *hMuQualDiff_;
   TH1I *hMuIsoDiff_;
+
+  TH1I *hGmtEvtBx_;
+  TH2F *h2GmtEtaPhi_;
 };
 
 
@@ -179,12 +195,15 @@ L1MuonAnalyzer::L1MuonAnalyzer(const edm::ParameterSet& iConfig) :
   emtfTag_(iConfig.getParameter<edm::InputTag>("emtfTag")),
   ugmtEmulatorTag_(iConfig.getParameter<edm::InputTag>("ugmtEmulatorTag")),
   ugmtUnpackerTag_(iConfig.getParameter<edm::InputTag>("ugmtUnpackerTag")),
+  gmtTag_(iConfig.getParameter<edm::InputTag>("gmtTag")),
   debug_(iConfig.getUntrackedParameter<bool>("debug", false)),
+  suppressCommas_(iConfig.getUntrackedParameter<bool>("commaSuppression", false)),
   analyzeBmtf_(false),
   analyzeOmtf_(false),
   analyzeEmtf_(false),
   analyzeUgmtEmulator_(false),
   analyzeUgmtUnpacker_(false),
+  analyzeGmt_(false),
   bmtfHistos_(fs_, "Bmtf", "BMTF"),
   omtfHistos_(fs_, "Omtf", "OMTF"),
   emtfHistos_(fs_, "Emtf", "EMTF"),
@@ -211,6 +230,10 @@ L1MuonAnalyzer::L1MuonAnalyzer(const edm::ParameterSet& iConfig) :
     analyzeUgmtUnpacker_ = true;
     consumes<l1t::MuonBxCollection>(ugmtUnpackerTag_);
   }
+  if (gmtTag_.label() != "none") {
+    analyzeGmt_ = true;
+    consumes<L1MuGMTReadoutCollection>(gmtTag_);
+  }
 
   hMuSizeDiff_ = fs_->make<TH1I>("hMuSizeDiff", "Emulator muon size - unpacker muon size for BX 0", 17, -8, 9);
   hMuPtDiff_ = fs_->make<TH1I>("hMuPtDiff", "Emulator muon pT - unpacker muon pt for BX 0", 521, -260, 261);
@@ -220,6 +243,9 @@ L1MuonAnalyzer::L1MuonAnalyzer(const edm::ParameterSet& iConfig) :
   hMuChargeValidDiff_ = fs_->make<TH1I>("hMuChargeValidDiff", "Emulator muon chargeValid - unpacker muon chargeValid for BX 0", 3, -1, 2);
   hMuQualDiff_ = fs_->make<TH1I>("hMuQualDiff", "Emulator muon qual - unpacker muon qual for BX 0", 31, -15, 16);
   hMuIsoDiff_ = fs_->make<TH1I>("hMuIsoDiff", "Emulator muon iso - unpacker muon iso for BX 0", 7, -3, 4);
+
+  hGmtEvtBx_ = fs_->make<TH1I>("hGmtEvtBx", "GMT event BX", 370, 0, 3700);
+  h2GmtEtaPhi_ = fs_->make<TH2F>("h2GmtEtaPhi", "GMT eta phi map;#eta;#phi", 100, -2.5, 2.5, 128, 0., 6.4);
 }
 
 
@@ -281,6 +307,34 @@ L1MuonAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
       return;
     }
   }
+  if (analyzeGmt_) {
+    edm::Handle<L1MuGMTReadoutCollection> gmtReadoutColl;
+    iEvent.getByLabel(gmtTag_, gmtReadoutColl);
+    if (gmtReadoutColl.isValid()) {
+      L1MuGMTReadoutCollection const* gmtrc = gmtReadoutColl.product();
+      std::vector<L1MuGMTReadoutRecord> gmt_records = gmtrc->getRecords();
+      std::vector<L1MuGMTReadoutRecord>::const_iterator igmtrr;
+      for(igmtrr=gmt_records.begin(); igmtrr!=gmt_records.end(); igmtrr++) {
+
+        if(igmtrr->getBxInEvent()==0) {
+          hGmtEvtBx_->Fill(igmtrr->getBxNr());
+        }
+
+        std::vector<L1MuGMTExtendedCand>::const_iterator gmt_iter;
+        std::vector<L1MuGMTExtendedCand> exc = igmtrr->getGMTCands();
+        unsigned int n = 0;
+        for(gmt_iter=exc.begin(); gmt_iter!=exc.end(); gmt_iter++) {
+          if (n < 12 && !(*gmt_iter).empty()) {
+            h2GmtEtaPhi_->Fill((*gmt_iter).etaValue(), (*gmt_iter).phiValue());
+            ++n;
+          }
+        }
+      }
+    } else {
+      edm::LogError("MissingProduct") << "GMT GMT readout collection not found" << std::endl;
+      return;
+    }
+  }
 
   // emulator - unpacker comparisons
   if (analyzeUgmtEmulator_ && analyzeUgmtUnpacker_) {
@@ -311,26 +365,35 @@ void
 L1MuonAnalyzer::analyzeRegionalMuonCand(const l1t::RegionalMuonCandBxCollection& muons, TfHistos& histos, const std::string& label)
 {
   if (debug_) {
-    std::cout << label << ": " << std::endl;
-    std::cout << label << " muon bx range: " << muons.getFirstBX() << " " << muons.getLastBX() << std::endl;
+    std::cout << "*** " << label << " muon bx range: " << muons.getFirstBX() << " " << muons.getLastBX() << std::endl;
   }
   for (int bx = muons.getFirstBX(); bx <= muons.getLastBX(); ++bx) {
-    if (debug_) std::cout << "BX " << bx << ": " << muons.size(bx) << " muons" << std::endl;
+    if (debug_ && muons.size(bx) > 0) std::cout << "    BX " << bx << ": " << muons.size(bx) << " muons" << std::endl;
     unsigned int ctr = 0;
     for (auto mu = muons.begin(bx); mu != muons.end(bx) && ctr < muons.size(bx); ++mu, ++ctr) {
-      histos.FillBx(bx);
-      histos.FillMuon(*mu);
+      // suppress comma events
+      if (suppressCommas_ && ((mu->dataword() & 0xFFFFFFFF) == 0x505050bc || (mu->dataword() >> 32) == 0x505050bc)) {
+        if (debug_) {
+          std::cout << "       COMMA   ";
+        }
+      } else {
+        histos.FillBx(bx);
+        histos.FillMuon(*mu);
+        if (debug_) {
+          std::cout << "       ";
+        }
+      }
 
       if (debug_) {
-        std::cout << "   hwPt: " << mu->hwPt()
+        std::cout << "hwPt: " << mu->hwPt()
                   << "   hwEta: " << mu->hwEta()
                   << "   hwPhi: " << mu->hwPhi()
                   << "   hwGlobalPhi: " << l1t::MicroGMTConfiguration::calcGlobalPhi(mu->hwPhi(), mu->trackFinderType(), mu->processor())
                   << "   hwSign: " << mu->hwSign()
                   << "   hwSignValid: " << mu->hwSignValid()
                   << "   hwQual: " << mu->hwQual()
-                  << "   stored dataword: " << hex << mu->dataword() << dec
-                  << "   dataword: " << hex << l1t::RegionalMuonRawDigiTranslator::generate64bitDataWord(*mu) << dec << std::endl;
+                  << "   stored dataword: 0x" << hex << setw(16) << setfill('0') << mu->dataword() << dec
+                  << "   dataword: 0x" << hex << setw(16) << setfill('0') << l1t::RegionalMuonRawDigiTranslator::generate64bitDataWord(*mu) << dec << std::endl;
       }
     }
   }
@@ -340,25 +403,24 @@ void
 L1MuonAnalyzer::analyzeMuon(const l1t::MuonBxCollection& muons, UGmtHistos& histos, const std::string& label)
 {
   if (debug_) {
-    std::cout << label << ": " << std::endl;
-    std::cout << label << " muon bx range: " << muons.getFirstBX() << " " << muons.getLastBX() << std::endl;
+    std::cout << "*** " << label << " muon bx range: " << muons.getFirstBX() << " " << muons.getLastBX() << std::endl;
   }
   for (int bx = muons.getFirstBX(); bx <= muons.getLastBX(); ++bx) {
-    if (debug_) std::cout << "BX " << bx << ": " << muons.size(bx) << " muons" << std::endl;
+    if (debug_ && muons.size(bx) > 0) std::cout << "    BX " << bx << ": " << muons.size(bx) << " muons" << std::endl;
     unsigned int ctr = 0;
     for (auto mu = muons.begin(bx); mu != muons.end(bx) && ctr < muons.size(bx); ++mu, ++ctr) {
       histos.FillBx(bx);
       histos.FillMuon(*mu);
 
       if (debug_) {
-        std::cout << "   hwPt: " << mu->hwPt()
+        std::cout << "       hwPt: " << mu->hwPt()
                   << "   hwEta: " << mu->hwEta()
                   << "   hwPhi: " << mu->hwPhi()
                   << "   hwCharge: " << mu->hwCharge()
                   << "   hwChargeValid: " << mu->hwChargeValid()
                   << "   hwQual: " << mu->hwQual()
                   << "   hwIso: " << mu->hwIso()
-                  << "   dataword: " << hex << l1t::MuonRawDigiTranslator::generate64bitDataWord(*mu) << dec << std::endl;
+                  << "   dataword: 0x" << hex << setw(16) << setfill('0') << l1t::MuonRawDigiTranslator::generate64bitDataWord(*mu) << dec << std::endl;
       }
     }
   }
